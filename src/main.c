2683364a5a75c5737275ad1ba02d0894edc21232
Chris Schlitt <chs5056@icloud.com>
2016-04-05 21:49:48 -0400

    Compiling on Aplite

--- src/main.c
+++ src/main.c
@@ -953,7 +953,8 @@ void init(void) {
 	app_message_register_inbox_dropped(in_dropped_handler); 
 	app_message_register_outbox_failed(out_failed_handler);
 		
-	app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
+	// app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
+	app_message_open(app_message_inbox_size_maximum(), 200);
 	
 }
 
